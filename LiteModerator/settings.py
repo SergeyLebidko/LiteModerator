3a39295fd784ae3d27ce40e96ac7cc6db4328be9
SergeyLebidko <it.sergeyler@mail.ru>
2019-11-26 14:36:23 +0300

    Исправлена настройка временной зоны

--- LiteModerator/settings.py
+++ LiteModerator/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
