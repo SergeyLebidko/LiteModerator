08791d4e7357d616edc6681fc184db79b835e6db
SergeyLebidko <it.sergeyler@mail.ru>
2019-11-29 16:50:29 +0300

    Добавлена команда rq_log

--- main/management/commands/rqlog.py
+++ main/management/commands/rqlog.py
@@ -1,7 +1,19 @@
 from django.core.management.base import BaseCommand
 
+from datetime import datetime
+from rq_scheduler import Scheduler
+from redis import Redis
+import rq
+
+from main.tasks import rq_task
+
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        print('RQLOG COMMAND')
+        queue = rq.Queue('rq_log', connection=Redis())
+        queue.enqueue(rq_task)
+
+        scheduler = Scheduler(queue=queue, connection=Redis())
+        scheduler.schedule(scheduled_time=datetime.utcnow(), func=rq_task, interval=5, repeat=1)
+        scheduler.run()
